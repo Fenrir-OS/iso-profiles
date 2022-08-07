2a3c0c5cb5a06496137b9e395305befdccdd466f
Justin Metayer <justin.metayer@icom-cloud.com>
2022-08-07 15:14:08 +0200

    update workflow

--- build.sh
+++ build.sh
@@ -42,8 +42,10 @@ buildiso -p base -q
 mkdir /usr/share/artools/iso-profiles/fenrir
 EOF
 
+pushd livefs
 mount --bind fenrir livefs/usr/share/artools/iso-profiles/fenrir
 mount --bind iso livefs/home/artools-workspace/iso/fenrir
+popd
 
 cat <<EOF | chroot livefs /bin/bash -xe -
 buildiso -p fenrir -i runit
